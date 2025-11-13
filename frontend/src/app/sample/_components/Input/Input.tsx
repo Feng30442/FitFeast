ab28fae9aabc969c332f21f04464c72fdc0531f0
Feng30442 <Longwei.fenng@gmail.com>
2025-11-13 17:33:35 +0900

    フォルダを削除しました。

--- frontend/src/app/sample/_components/Input/Input.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Input.module.css";
-
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
-export const Input = (props: InputProps) => {
-  return <input className={styles.input} {...props} />;
-};
